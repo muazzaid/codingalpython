f5a2fe7c63bb50f3744171811dd7f3f813e6565e
muazzaid <josuepelembe47@gmail.com>
2025-05-23 17:50:40 +0200

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,21 @@
+# Pritnting greeting message
+print("what's up nigga")
+
+# Printing name 
+print("my name is muazadas")
+
+# printing facts about me 
+print(" i am a millionare")
+
+# printing my age 
+print("my age i the same as my money")
+
+#Priting number
+print(34)
+
+#next ma line after print
+print("hello python \n")
+
+#pritnting all keyboards present in python
+import keyword
+print(keyword.kwlist)
