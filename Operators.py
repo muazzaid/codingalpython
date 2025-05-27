59f13ca13dcf56ee30311f90ed02990228f3ded7
muazzaid <josuepelembe47@gmail.com>
2025-05-27 17:54:14 +0200

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,12 @@
+#Activity - Operators 
+
+num1 =int(input("Please enter num1:"))
+num2 =int(input("Please enter num2:"))
+
+sub = num1-num2
+print("Subtraction =", sub)
+
+
+
+sum = num1+num2
+print("Sum =", sum )
---
+++ datatype.py
@@ -0,0 +1,25 @@
+name = "muazadas"
+age = 109
+is_student = True
+weight = 70.5
+
+print("NAME:", name)
+print("Data type of name:", type(name))
+
+print("AGE:", age)
+print("Data type of age:", type(age))
+print("IS_STUDENT:", is_student)
+print("Data type of is_student:", type(is_student))
+print("WEIGHT:", weight)
+print("Data type of weight:", type(weight))
+
+print("\n After Type Casting...")
+age = str(age)
+print("AGE:", age)
+print("Data type of age after type casting:", type(age))
+weight = int(weight)
+print("WEIGHT:", weight)
+print("Data type of weight after type casting:", type(weight))
+
+
+
