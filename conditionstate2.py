500b68d4375e77b714605c81d99f2da6d1be4a74
muazzaid <josuepelembe47@gmail.com>
2025-05-30 18:38:18 +0200

    Add files via upload

---
+++ conditionstate2.py
@@ -0,0 +1,18 @@
+height = float(input("Enter your height in cm: "))
+weight = float(input("Enter your weight in kg: "))
+
+BMI = weight / ((height / 100) ** 2)
+print("your BMI is", BMI)
+
+if BMI < 18.4:
+    print("You are underweight")
+elif BMI <= 24.9:
+    print("You are normal weight")
+elif BMI <= 29.9:
+    print("You are overweight")
+elif BMI <= 34.9:
+    print("You are severly overweight")
+elif BMI <= 39.9:
+    print("You are obese")
+else:
+    print("You are severely obese")
