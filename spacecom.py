6c4b092d38dcfd405dcb75245594ce4c9851fa39
muazzaid <josuepelembe47@gmail.com>
2025-08-12 19:09:32 +0200

    Add files via upload

---
+++ spacecom.py
@@ -0,0 +1,16 @@
+def sum(n):
+    return n * (n + 1) // 2
+
+def arraysum(ar):
+    sum = 0
+    for i in a:
+        sum = sum + i
+
+        return(sum)
+    
+a = [1, 2, 3, 4, 5]
+arraysum(a)
+
+def summ(n):
+    if(n <= 0):
+        return n+ summ(n-1)
