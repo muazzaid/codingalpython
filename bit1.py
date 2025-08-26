98305d3fa3da11dd963bac1393b1e6e88d8f28f1
muazzaid <josuepelembe47@gmail.com>
2025-08-26 17:51:09 +0200

    Add files via upload

---
+++ bit1.py
@@ -0,0 +1,12 @@
+def numberOfBits(n):
+
+    count = 0
+
+    while (n):
+        count += 1
+        n >>= 1
+
+    return count
+
+number = int(input("Enter a number: "))
+print("Total bits :", numberOfBits(number))
---
+++ bit2.py
@@ -0,0 +1,14 @@
+num1 =10
+num2 =20
+
+print("num1 & num2 =", num1 & num2)
+
+print("num1 | num2 =", num1 | num2)
+
+print("~num1 =", ~num1)
+
+print("num1 ^ num2 =", num1 ^ num2)
+
+print("num1 << 2 =", num1 << 2)
+
+print("num2 >> 2 =", num2 >> 2)
