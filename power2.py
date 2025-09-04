ead7f94136d9a0c5d324ea2968f30a8165326c08
muazzaid <josuepelembe47@gmail.com>
2025-09-04 19:33:55 +0200

    Add files via upload

---
+++ power2.py
@@ -0,0 +1,17 @@
+def powerOf4(number):
+    
+  count = 0
+if (  number (~(number & (number - 1)))):
+  
+    while (number > 1):
+      number = number >> 1
+      count += 1
+      
+if (count % 2 == 0):
+          True
+else:
+         False
+
+number = int(input("Enter a number: "))
+if(powerOf4(number)):
+    print("\nThe number is a power of 4")
